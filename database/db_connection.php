fc446499521d572197f2bf3d30f55c902e4af2a7
Bibin <bibin3@gmail.com>
2021-06-06 08:27:58 +0530

    initial commit

---
+++ database/db_connection.php
@@ -0,0 +1,26 @@
+<?php
+
+function OpenCon()
+{
+   $hostname = "localhost";
+
+   $user_name = "root";
+   $password = "Apple$123";
+   $db = "24delivery";
+
+   /*$user_name = "u246501751_24delivery";
+   $password = "Apple$123";
+   $db = "u246501751_24delivery";*/
+   
+
+
+   $conn = new mysqli($hostname, $user_name, $password, $db) or die("Connect failed: %s\n" . $conn->error);
+
+
+   return $conn;
+}
+
+function CloseCon($conn)
+{
+   $conn->close();
+}
